a77b6ac7ae566c9ebe44755b2a746cdb38c232d1
Grigoriy Kapranov <kapranovgu@mail.ru>
2023-01-24 21:29:19 +0300

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите цикл, который принимает 
+на вход два числа (A и B) 
+и возводит число A в натуральную степень B. 
+Реализовать через функции.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int numberA = Prompt("Введите число А");
+int numberB = Prompt("Введите число B");
+
+System.Console.WriteLine($"Число {numberA} в степени {numberB} = {numberDegree(numberA, numberB)}");
+
+double numberDegree(int firstNumber, int secondNumber)
+{
+    double result = Math.Pow(firstNumber, secondNumber);
+    return result;
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message + "> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
